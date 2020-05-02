8c7d46ce1bcc53f7d60f55210797cf325f13b270
manqobasukati <manqoba.sukati@peoplesized.com>
2020-05-02 14:51:21 +0200

    refactor View to MyView

--- prizelist/prize_list_app/urls.py
+++ prizelist/prize_list_app/urls.py
@@ -12,7 +12,9 @@ router.register(r'prize-list-item',views.PrizeListItemViewSet)
 urlpatterns = [
     path('',include(router.urls)),
     path('shops/<int:pk>/branches',views.BranchView.as_view()),
+    path('shops/<int:pk>/branches/<int:branch_id>',views.BranchDetailView.as_view()),
     path('shops/<int:shop_id>/branches/<int:branch_id>/prize-lists/', views.PrizeListView.as_view()),
     path('shops/<int:shop_id>/branches/<int:branch_id>/orders/', views.OrderView.as_view()),
+    path('shops/<int:shop_id>/branches/<int:branch_id>/orders/<int:order_id>', views.OrderDetailView.as_view()),
     path('shops/<int:shop_id>/branches/<int:branch_id>/prize-lists/<int:prize_list_id>', views.PrizeListViewDetail.as_view()),
 ]
--- prizelist/prize_list_app/views.py
+++ prizelist/prize_list_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import Http404
 
 from rest_framework.parsers import FileUploadParser
 from rest_framework.response import Response
@@ -17,127 +18,7 @@ class ShopViewSet(viewsets.ModelViewSet):
     queryset = Shop.objects.all()
     serializer_class = ShopSerializer
 
-
-class BranchView(APIView):
-    queryset = Branch.objects.all()
-    serializer_class = BranchSerializer
-
-    def get_shop(self, pk):
-        try:
-            return Shop.objects.get(pk=pk)
-        except Branch.DoesNotExist:
-            raise Http404
-
-    def get(self,request,pk):
-       shop = self.get_shop(pk)
-       branches = Branch.objects.filter(shop=shop)
-       serializer = BranchSerializer(branches, many=True)
-       return Response(serializer.data)
-
-    def post(self,request,pk):
-        serializer = BranchSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class PrizeListView(APIView):
-    parser_class = (FileUploadParser,)
-    queryset = PrizeList.objects.all()
-    serializer_class = PrizeListSerializer
-
-    def get_object(self, pk):
-        try:
-            return PrizeList.objects.get(pk=pk)
-        except PrizeList.DoesNotExist:
-            raise Http404
-
-    def get_branch(self,pk):
-        try:
-            return Branch.objects.get(pk=pk)
-        except Branch.DoesNotExist:
-            raise Http404
-
-    def get_shop(self, pk):
-        try:
-            return Shop.objects.get(pk=pk)
-        except Shop.DoesNotExist:
-            raise Http404
-
-    def post(self, request, *args, **kwargs):
-
-        serializer = PrizeListSerializer(data=request.data)
-        my_file =  request.FILES['prize_list']
-        if serializer.is_valid():
-            prize_list_data = get_items(file_name=my_file)
-            serializer_object = serializer.save()
-            prize_list = self.get_object(serializer_object.id)
-            for i in prize_list_data:
-                prize_list_item = PrizeListItem(category=i['category'],label=i['label'],
-                                                prize=i['prize'],prize_list=prize_list,shop=self.get_shop(kwargs['shop_id']),
-                                                branch=self.get_branch(kwargs['branch_id'])
-                                                )
-                prize_list_item.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-
-
-
-    def get(self, request,**kwargs):
-        shop = self.get_shop(kwargs['shop_id'])
-        branch = self.get_branch(kwargs['branch_id'])
-        prize_list = PrizeList.objects.filter(branch=branch)
-        serializer = PrizeListSerializer(prize_list, many=True)
-        return Response(serializer.data)
-
-class PrizeListViewDetail(APIView):
-    parser_class = (FileUploadParser,)
-    serializer_class = PrizeListSerializer
-
-    def get_object(self, pk):
-        try:
-            return PrizeList.objects.get(pk=pk)
-        except PrizeList.DoesNotExist:
-            raise Http404
-
-    def get_branch(self,pk):
-        try:
-            return Branch.objects.get(pk=pk)
-        except Branch.DoesNotExist:
-            raise Http404
-
-    def get_shop(self, pk):
-        try:
-            return Shop.objects.get(pk=pk)
-        except Shop.DoesNotExist:
-            raise Http404
-
-    def get_prize_list(self,pk):
-        try:
-            return PrizeList.objects.get(pk=pk)
-        except Shop.DoesNotExist:
-            raise Http404
-
-    def get(self,request,**kwargs):
-        prize_list = self.get_prize_list(kwargs['prize_list_id'])
-        serializer = PrizeListSerializer(prize_list)
-        return Response(serializer.data)
-
-class BuyerViewSet(viewsets.ModelViewSet):
-    queryset = Buyer.objects.all()
-    serializer_class = BuyerSerializer
-
-class PrizeListItemViewSet(viewsets.ModelViewSet):
-    queryset = PrizeListItem.objects.all()
-    serializer_class = PrizeListItemSerializer
-
-class OrderView(APIView):
-    queryset = Order.objects.all()
-    serializer_class = OrderSerializer
+class MyView:
 
     def get_object(self, pk):
         try:
@@ -169,6 +50,90 @@ class OrderView(APIView):
         except Order.DoesNotExist:
             raise Http404
 
+    def get_prize_list(self,pk):
+        try:
+            return PrizeList.objects.get(pk=pk)
+        except PrizeList.DoesNotExist:
+            raise Http404
+
+
+class BranchView(APIView, MyView):
+    queryset = Branch.objects.all()
+    serializer_class = BranchSerializer
+
+    def get(self,request,pk):
+       shop = self.get_shop(pk)
+       branches = Branch.objects.filter(shop=shop)
+       serializer = BranchSerializer(branches, many=True)
+       return Response(serializer.data)
+
+    def post(self,request,pk):
+        serializer = BranchSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class BranchDetailView(APIView, MyView):
+
+    def get(self, request,**kwargs):
+        branch = self.get_branch(kwargs['branch_id'])
+        serializer = BranchSerializer(branch)
+        return Response(serializer.data)
+
+
+class PrizeListView(APIView, MyView):
+    parser_class = (FileUploadParser,)
+    queryset = PrizeList.objects.all()
+    serializer_class = PrizeListSerializer
+
+    def post(self, request, *args, **kwargs):
+
+        serializer = PrizeListSerializer(data=request.data)
+        my_file =  request.FILES['prize_list']
+        if serializer.is_valid():
+            prize_list_data = get_items(file_name=my_file)
+            serializer_object = serializer.save()
+            prize_list = self.get_object(serializer_object.id)
+            for i in prize_list_data:
+                prize_list_item = PrizeListItem(category=i['category'],label=i['label'],
+                                                prize=i['prize'],prize_list=prize_list,shop=self.get_shop(kwargs['shop_id']),
+                                                branch=self.get_branch(kwargs['branch_id'])
+                                                )
+                prize_list_item.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def get(self, request,**kwargs):
+        shop = self.get_shop(kwargs['shop_id'])
+        branch = self.get_branch(kwargs['branch_id'])
+        prize_list = PrizeList.objects.filter(branch=branch)
+        serializer = PrizeListSerializer(prize_list, many=True)
+        return Response(serializer.data)
+
+class PrizeListViewDetail(APIView,MyView):
+   
+    serializer_class = PrizeListSerializer
+
+    def get(self,request,**kwargs):
+        prize_list = self.get_prize_list(kwargs['prize_list_id'])
+        serializer = PrizeListSerializer(prize_list)
+        return Response(serializer.data)
+
+class BuyerViewSet(viewsets.ModelViewSet):
+    queryset = Buyer.objects.all()
+    serializer_class = BuyerSerializer
+
+class PrizeListItemViewSet(viewsets.ModelViewSet):
+    queryset = PrizeListItem.objects.all()
+    serializer_class = PrizeListItemSerializer
+
+class OrderView(APIView,MyView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
     def get(self,request,**kwargs):
         orders = Order.objects.filter(shop=self.get_shop(kwargs['shop_id']),
                                         branch=self.get_branch(kwargs['branch_id']))
@@ -193,34 +158,14 @@ class OrderView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-
-
-
-class OrderItemView(APIView):
+class OrderDetailView(APIView,MyView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
 
-    def get_object(self, pk):
-        try:
-            return PrizeList.objects.get(pk=pk)
-        except PrizeList.DoesNotExist:
-            raise Http404
-
-    def get_branch(self,pk):
-        try:
-            return Branch.objects.get(pk=pk)
-        except Branch.DoesNotExist:
-            raise Http404
-
-    def get_shop(self, pk):
-        try:
-            return Shop.objects.get(pk=pk)
-        except Shop.DoesNotExist:
-            raise Http404
-
     def get(self, request,**kwargs):
-        items = OrderItem.objects.filter(shop=self.get_shop(kwargs['shop_id']),
-                                        branch=self.get_branch(kwargs['branch_id']),)
-        serializer = OrderItemSerializer(items,many=True)
+        order = self.get_order(kwargs['order_id'])
+        serializer = OrderSerializer(order)
         return Response(serializer.data)
+
+
+
